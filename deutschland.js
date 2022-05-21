b2d8608c91ed59e527d58ff5cd149125495327d5
BuildTools <unconfigured@null.spigotmc.org>
2022-05-21 17:55:15 +0200

    Some Javascript

--- deutschland.js
+++ deutschland.js
@@ -3,10 +3,29 @@
 // 1 - neuer Wert
 // 90 * neuster Wert
 
+async function getApiData() {
+    let response = await fetch("https://api.corona-zahlen.org/states");
+    let data = await response.json();
+    return data;
+}
+
+
 var colortest = "hue-rotate(" + 90 +"deg)"
 
 function changeColor() {
     document.getElementById("background").style.filter = colortest;
 }
 
+function changeListHeight() {
+    var img = document.getElementById("background");
+    var currHeight = img.clientHeight;
+    document.getElementById("bundesländer-liste").style.height = currHeight + "px";
+}
+
+
 window.onload = changeColor;
+window.onload = changeListHeight;
+
+window.onload = getApiData;
+
+window.addEventListener('resize', changeListHeight);
